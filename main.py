d08d7e5eeef1b08e4f0d98d2ab2687e77ca62613
viperasi <viperasi@gmail.com>
2013-11-08 00:42:53 +0800

    管理平台修改更新

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, session, render_template, request, redirect, url_for
+from flask import Flask, session, render_template, request, redirect, url_for, abort
 from db import db_session
 from models import Card
 from sqlalchemy import func
@@ -13,55 +13,139 @@ PER_PAGE = 10
 def shutdown_session(exception=None):
     db_session.remove()
 
+
+#mgr start
 @app.route('/mgr')
 @app.route('/mgr/index')
 @app.route('/mgr/index/<int:page>')
-def index(page=1):
+def mgrIndex(page=1):
     route = 'index'
     result = getCards(page, None)
-    return render_template('index.html', cards=result['cards'], allPage=result['allPage'], page=result['page'], route=route)
+    return render_template('mgr/index.html', cards=result['cards'], allPage=result['allPage'], page=result['page'], route=route)
 
 @app.route('/mgr/hero')
 @app.route('/mgr/hero/<int:page>')
-def hero(page=1):
+def mgrHero(page=1):
     route = 'hero'
     result = getCards(page, u'英雄')
-    return render_template('index.html', cards=result['cards'], allPage=result['allPage'], page=result['page'], route=route)
+    return render_template('mgr/index.html', cards=result['cards'], allPage=result['allPage'], page=result['page'], route=route)
 
 @app.route('/mgr/hskill')
 @app.route('/mgr/hskill/<int:page>')
-def heroskill(page=1):
+def mgrHeroSkill(page=1):
     route = 'hskill'
     result = getCards(page, u'英雄技能')
-    return render_template('index.html', cards=result['cards'], allPage=result['allPage'], page=result['page'], route=route)
+    return render_template('mgr/index.html', cards=result['cards'], allPage=result['allPage'], page=result['page'], route=route)
 
 @app.route('/mgr/weapon')
 @app.route('/mgr/weapon/<int:page>')
-def weapon(page=1):
+def mgrWeapon(page=1):
     route = 'weapon'
     result = getCards(page, u'武器')
-    return render_template('index.html', cards=result['cards'], allPage=result['allPage'], page=result['page'],  route=route)
+    return render_template('mgr/index.html', cards=result['cards'], allPage=result['allPage'], page=result['page'],  route=route)
 
 @app.route('/mgr/skill')
 @app.route('/mgr/skill/<int:page>')
-def skill(page=1):
+def mgrSkill(page=1):
     route = 'skill'
     result = getCards(page, u'技能')
-    return render_template('index.html', cards=result['cards'], allPage=result['allPage'], page=result['page'],  route=route)
+    return render_template('mgr/index.html', cards=result['cards'], allPage=result['allPage'], page=result['page'],  route=route)
 
 @app.route('/mgr/ally')
 @app.route('/mgr/ally/<int:page>')
-def ally(page=1):
+def mgrAlly(page=1):
     route = 'ally'
     result = getCards(page, u'随从')
-    return render_template('index.html', cards=result['cards'], allPage=result['allPage'], page=result['page'],  route=route)
+    return render_template('mgr/index.html', cards=result['cards'], allPage=result['allPage'], page=result['page'],  route=route)
+
+@app.route('/mgr/edit/<int:id>')
+def mgrEdit(id=None):
+    if id is not None:
+        card = Card.query.filter(Card.id == id).first()
+        if card is not None:
+            return render_template('mgr/edit.html', card=card)
+    abort(404)
+
+@app.route('/mgr/save/<int:id>', methods=['POST'])
+def mgrSave(id=None):
+    if id is not None:
+        card = Card.query.filter(Card.id == id).first()
+        if card is not None:
+            if request.form['name'] is not None and request.form['name'] != 'None':
+                card.card_name = request.form['name']
+            if request.form['engname'] is not None and request.form['engname'] != 'None':
+                card.card_engname = request.form['engname']
+            if request.form['type'] is not None and request.form['type'] != 'None':
+                card.card_type = request.form['type']
+            if request.form['rarity'] is not None and request.form['rarity'] != 'None':
+                card.card_rarity = request.form['rarity']
+            if request.form['class'] is not None and request.form['class'] != 'None':
+                card.card_class = request.form['class']
+            if request.form['set'] is not None and request.form['set'] != 'None':
+                card.card_set = request.form['set']
+            if request.form['race'] is not None and request.form['race'] != 'None':
+                card.card_race = request.form['race']
+            if request.form['faction'] is not None and request.form['faction'] != 'None':
+                card.card_faction = request.form['faction']
+            if request.form['collectible'] is not None and request.form['collectible'] != 'None':
+                card.card_collectible = request.form['collectible']
+            if request.form['crafting'] is not None and request.form['crafting'] != 'None':
+                card.card_crafting = request.form['crafting']
+            if request.form['gained'] is not None and request.form['gained'] != 'None':
+                card.card_gained = request.form['gained']
+            if request.form['artist'] is not None and request.form['artist'] != 'None':
+                card.card_artist = request.form['artist']
+            if request.form['cost'] is not None and request.form['cost'] != 'None':
+                card.card_cost = request.form['cost']
+            if request.form['att'] is not None and request.form['att'] != 'None':
+                card.card_att = request.form['att']
+            if request.form['hp'] is not None and request.form['hp'] != 'None':
+                card.card_hp = request.form['hp']
+            if request.form['img'] is not None and request.form['img'] != 'None':
+                card.card_img = request.form['img']
+            if request.form['engimg'] is not None and request.form['engimg'] != 'None':
+                card.card_engimg = request.form['engimg']
+            if request.form['desc'] is not None and request.form['desc'] != 'None':
+                card.card_desc = request.form['desc']
+            if request.form['engdesc'] is not None and request.form['engdesc'] != 'None':
+                card.card_engdesc = request.form['engdesc']
+            if request.form['remark'] is not None and request.form['remark'] != 'None':
+                card.card_remark = request.form['remark']
+            if request.form['engremark'] is not None and request.form['engremark'] != 'None':
+                card.card_engremark = request.form['engremark']
+            db_session.merge(card)
+            db_session.commit()
+            return redirect(url_for('mgrIndex'))
+    abort(404)
+#mgr end
+
+#index start
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/set')
+def set():
+    heros = Card.query.filter(Card.card_type == u'英雄', Card.card_set == u'基础')
+    return render_template('set.html', heros=heros)
+
+@app.route('/setstart/<int:id>')
+def setStart(id=None):
+    if id is not None:
+        card = Card.query.filter(Card.id == id).first()
+        classcards = Card.query.filter(Card.card_class == card.card_class, Card.card_type != u'英雄', Card.card_type != u'英雄技能')
+        allycards = Card.query.filter(Card.card_type == u'随从')
+        return render_template('setStart.html', card=card, classcards=classcards, allycards=allycards)
+    return redirect(url_for('set'))
+
+#index end
 
 @app.route('/card/<int:id>')
 def card(id=None):
     if id is not None:
         card = Card.query.filter(Card.id == id).first()
-
         return simplejson.dumps(card.__json__())
+    abort(404)
 
 def getCards(page=1, type=None):
     result = {
